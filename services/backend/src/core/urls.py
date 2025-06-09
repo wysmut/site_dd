37451dc2bd06d8c0175f306ac902afb7440d7f18
Dmitrii Semenov <info@dxyzsem.ru>
2025-06-09 14:28:24 +0700

    Update urls.py

--- services/backend/src/core/urls.py
+++ services/backend/src/core/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('test/', views.test_view, name='test'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+]
