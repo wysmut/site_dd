ed764ca75a8606feab2493fbdefabd732d7aaff9
Dmitrii Semenov <info@dxyzsem.ru>
2025-06-09 14:25:41 +0700

    Update init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+docker-compose up -d db
+sleep 10
+docker-compose exec db psql -U user -d dbname -c "CREATE DATABASE dbname;"
+docker-compose exec db psql -U user -d dbname -c "CREATE USER user WITH PASSWORD 'password';"
+docker-compose exec db psql -U user -d dbname -c "ALTER ROLE user SET client_encoding TO 'utf8';"
+docker-compose exec db psql -U user -d dbname -c "ALTER ROLE user SET default_transaction_isolation TO 'read committed';"
+docker-compose exec db psql -U user -d dbname -c "ALTER ROLE user SET timezone TO 'UTC';"
+docker-compose exec db psql -U user -d dbname -c "GRANT ALL PRIVILEGES ON DATABASE dbname TO user;"
