c86a4291f814afc1e63eb88e11f07b5a4f3c49b1
Dmitrii Semenov <info@dxyzsem.ru>
2025-06-09 15:25:29 +0700

    Update urls.py

--- services/backend/src/config/urls.py
+++ services/backend/src/config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('api/', include('api.urls')),
+path('admin/', admin.site.urls),
+path('', include('core.urls')),
 ]
