55fa218f49f2ae620dd2e3b8a97f7d688d86cd86
Dmitrii Semenov <info@dxyzsem.ru>
2025-06-09 14:31:14 +0700

    Update app.py

--- services/dash_app/src/app.py
+++ services/dash_app/src/app.py
@@ -1,1 +1,12 @@
+import dash
+from dash import dcc, html
+import plotly.express as px
+import requests
 
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+    dcc.Graph(id='progress-graph'),
+    dcc.Interval(id='interval', interval=30000, n_intervals=0)
+])
