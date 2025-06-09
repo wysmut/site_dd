60eec2023778a89d77623eddd08c800a7e8bd65c
Dmitrii Semenov <info@dxyzsem.ru>
2025-06-09 14:27:57 +0700

    Update models.py

--- services/backend/src/core/models.py
+++ services/backend/src/core/models.py
@@ -1,1 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    is_teacher = models.BooleanField(default=False)
+
+class Test(models.Model):
+    title = models.CharField(max_length=255)
+    level = models.CharField(max_length=50)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='questions')
+    text = models.TextField()
+    option1 = models.CharField(max_length=255)
+    option2 = models.CharField(max_length=255)
+    option3 = models.CharField(max_length=255)
+    option4 = models.CharField(max_length=255)
+    correct_option = models.PositiveSmallIntegerField()
+
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    score = models.FloatField()
+    date_taken = models.DateTimeField(auto_now_add=True)
