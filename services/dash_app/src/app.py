56ca6929d6dedb0952ec60b877563fff7dd08966
Dmitrii Semenov <info@dxyzsem.ru>
2025-06-09 14:31:49 +0700

    Update app.py

--- services/dash_app/src/app.py
+++ services/dash_app/src/app.py
@@ -1,12 +1,15 @@
 import dash
 from dash import dcc, html
-import plotly.express as px
-import requests
+from .callbacks import register_callbacks
+import os
 
 app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
     dcc.Graph(id='progress-graph'),
     dcc.Interval(id='interval', interval=30000, n_intervals=0)
 ])
+
+register_callbacks(app)
